c159dbde8aa97eb0e1f93fa638225b3df07397f0
SantiagoCalderon1999 <santiagocalderon1999@gmail.com>
2024-04-11 22:38:42 +0100

    Add second addition lemma

--- RealAnalysis/Addition.thy
+++ RealAnalysis/Addition.thy
@@ -9,8 +9,8 @@ fun Sum::"nat \<Rightarrow> nat \<Rightarrow> nat" where
 
 value "Sum 3 9"
 
-text \<open>Addition basic theorem\<close>
-lemma "Sum n 0 = n"
+text \<open>Addition basic lemma part 1, proof by induction\<close>
+lemma addition_1: "Sum n 0 = n"
 proof(induction n)
   case 0
   have 1: "Sum 0 0 = 0"
@@ -28,6 +28,40 @@ next
 
   also have 2: "... = Suc n"
     using Suc.IH
+    ..
+
+  finally show ?case
+    .
+qed
+
+
+text \<open>Addition basic lemma part 2, proof by induction\<close>
+lemma "Sum n (Suc m) = Suc (Sum n m)"
+proof(induction n)
+  case 0
+  have 1: "Sum 0 (Suc m) = Suc m"
+    using  Sum.simps(1)
+    .
+
+  also have 2: "... = Suc (Sum 0 m)"
+    using Sum.simps(1)[symmetric]
+    ..
+
+  finally show ?case
+    .
+
+next
+  case (Suc n)
+  have 1: "Sum (Suc n) (Suc m) = Suc (Sum n (Suc m))"
+    using Sum.simps(2)
+    .
+
+  also have 2: "... = Suc (Suc (Sum n m))"
+    using Suc.IH
+    ..
+
+  also have 3: "... = Suc (Sum (Suc n) m)"
+    using Sum.simps(2)[symmetric]
     ..
 
   finally show ?case
